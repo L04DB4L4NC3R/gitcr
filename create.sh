152d2dabeee385610ef832059c607342cbdd3980
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-01-23 00:56:54 +0530

    print complete JSON

--- create.sh
+++ create.sh
@@ -7,30 +7,54 @@ template_owner=$GITHUB_TEMPLATE_OWNER
 owner_name=$GITHUB_ORGANIZATION_OWNER
 token=$GITHUB_PERSONAL_ACCESS_TOKEN
 
-echo "Running..."
+create(){
+	echo "Executing \"create\""
+	CREATION_STATUS=""
 
-while p= read -r line; do
-	
-	echo "<><><><><><><>"
-	echo "Repo: $line"
+	count=1
+	cur=1
+	result=("[")
+	while p= read -r line; do
+		
+		echo "Repo: $line"
 
-	# Making the API call and storing the resulting status code
-	code=`curl -i -s -XPOST -d "{\"owner\": \"$owner_name\", \"name\": \"$line\"}" https://api.github.com/repos/$template_owner/$template_name/generate -H "Accept: application/vnd.github.baptiste-preview+json" -H "Authorization: token $token" | head -1 | cut -d" " -f2`
+		# Making the API call and storing the resulting status code
+		res=`curl -i -s -XPOST -d "{\"owner\": \"$owner_name\", \"name\": \"$line\"}" https://api.github.com/repos/$template_owner/$template_name/generate -H "Accept: application/vnd.github.baptiste-preview+json" -H "Authorization: token $token" | head -1`
 
-	echo Status code: $code
+		code=`echo $res | cut -d" " -f2`
+		status_of_request=`echo $res | cut -d" " -f3-5`
 
-	# Checking if request succeeded
-	if [ $code -eq 201 ]
-	then
-		echo [Success] for repo \"$line\"
-		echo URL: https://github.com/$GITHUB_ORGANIZATION_OWNER/$line
-	else 
-		echo [FAILURE] for repo \"$line\"
-	fi
+		echo Status code: $code
 
-	echo "<><><><><><><>"
-	echo "Sleeping for 1.5s"
-	sleep 1.5 # For avoiding rate limit of 10 req per min
+		# Checking if request succeeded
+		if [[ $code -eq 201 ]]
+		then
+			echo [Success] for repo \"$line\"
+			echo URL: https://github.com/$GITHUB_ORGANIZATION_OWNER/$line
+			CREATION_STATUS=SUCCESS
+			url="https://github.com/$GITHUB_ORGANIZATION_OWNER/$line"
+		else 
+			CREATION_STATUS=FAILURE
+			echo [FAILURE] for repo \"$line\"
+		fi
+		# TODO: avoiding rate limit of 10 req per min
+		
+		result[$count]=`echo { \"repo_name\": \"$line\", \"status\": \"$CREATION_STATUS\", \"order_of_execution\": $cur, \"code\": $code, \"url\": \"$url\" } | jq`
 
-done < ./repos.txt
+		let "count++"
+		let "cur++"
+		result[$count]=","
+		let "count++"
 
+		url=""
+
+	done < ./repos.txt
+
+	result[$count]="]"
+	let "count=count-1"
+	result[$count]=""
+	echo ${result[@]}
+
+}
+
+create
